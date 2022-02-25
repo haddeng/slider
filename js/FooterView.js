34f6582dd30e65e792380ace3d22a6fb34b9c003
haddeng <50776800+haddeng@users.noreply.github.com>
2022-02-25 18:15:21 +0000

    Update FooterView.js

--- js/FooterView.js
+++ js/FooterView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 export default class FooterView extends View {
 	constructor() {
-		super('/html/footer.html', {
+		super('html/footer.html', {
 			year: new Date().getFullYear(),
 		});
 	}
